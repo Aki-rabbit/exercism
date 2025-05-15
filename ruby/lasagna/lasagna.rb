7f28eda51e6dd3907ef0f56bb5f2519e6aa344db
kakehi <poohaki0628@gmail.com>
2025-05-15 17:00:18 +0900

    amusement-park 解答前

--- ruby/lasagna/lasagna.rb
+++ ruby/lasagna/lasagna.rb
@@ -2,7 +2,7 @@ class Lasagna
   EXPECTED_MINUTES_IN_OVEN = 40
 
   def remaining_minutes_in_oven(actual_minutes_in_oven)
-    return EXPCTED_MINUTES_IN_OVEN - actual_minutes_in_oven
+    return EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
