8d570b74555fd95a381314df5b0c58d3224f86fb
kakehi <poohaki0628@gmail.com>
2025-05-15 16:09:57 +0900

    lasagna 解答②

--- ruby/lasagna/lasagna.rb
+++ ruby/lasagna/lasagna.rb
@@ -1,22 +1,23 @@
 class Lasagna
-  Lasagna::EXPECTED_MINUTES_IN_OVEN = 40
-
-
 
   def remaining_minutes_in_oven(actual_minutes_in_oven)
     raise 'Please implement the Lasagna#remaining_minutes_in_oven method'
+    return Lasagna::EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
     raise 'Please implement the Lasagna#preparation_time_in_minutes method'
+    return layers * 2
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
     raise 'Please implement the Lasagna#total_time_in_minutes method'
+    return self.remaining_minutes_in_oven(actual_minutes_in_oven) + self.preparation_time_in_minutes(number_of_layers)
   end
 end
 
+Lasagna::EXPECTED_MINUTES_IN_OVEN = 40
 lasagna = Lasagna.new
-    lasagna.remaining_minutes_in_oven(30)
-    lasagna.preparation_time_in_minutes(2)
-    lasagna.total_time_in_minutes(number_of_layers: 3, actual_minutes_in_oven: 20)
+lasagna.remaining_minutes_in_oven(30)
+lasagna.preparation_time_in_minutes(2)
+lasagna.total_time_in_minutes(number_of_layers: 2, actual_minutes_in_oven: 30)
