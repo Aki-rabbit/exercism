5777a58a49da16fccfd35613133c29ed5abb1035
kakehi <poohaki0628@gmail.com>
2025-05-16 14:42:43 +0900

    amusement-park-improvement final solituin

--- ruby/amusement-park-improvements/attendee.rb
+++ ruby/amusement-park-improvements/attendee.rb
@@ -14,7 +14,7 @@ class Attendee
   # Do not edit above methods, add your own methods below.
 
   def has_pass?
-    !pass_id.nill?
+    @pass_id != nil
   end
 
   def fits_ride?(ride_minimum_height)
