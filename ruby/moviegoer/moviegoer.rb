2613c313644ae641d2cf30019604567628961acb
kakehi aki <197594452+Aki-rabbit@users.noreply.github.com>
2025-06-13 13:26:50 +0900

    moviegoer 解答

--- ruby/moviegoer/moviegoer.rb
+++ ruby/moviegoer/moviegoer.rb
@@ -9,15 +9,23 @@ class Moviegoer
   end
 
   def ticket_price
-    raise 'Please implement the Moviegoer#ticket_price method'
+    #60歳以上の人はチケットが10.00ドルになります。
+    #それより若い人は、15.00ドルの全額を支払います。
+    @age >= 60? 10.00 : 15.00
   end
 
   def watch_scary_movie?
-    raise 'Please implement the Moviegoer#watch_scary_movie method'
+    #18歳以上であれば、怖い映画を観ることができます。
+    #それより若い場合、観ることはできません。
+    @age >= 18? true : false
   end
 
   # Popcorn is 🍿
   def claim_free_popcorn!
-    raise 'Please implement the Moviegoer#claim_free_popcorn method'
+    if @member == true
+      "🍿" 
+    else
+      raise NotMovieClubMemberError
+    end
   end
 end
