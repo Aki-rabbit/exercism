834f1659f8ca08d64341d80f866992d6f3477e90
kakehi aki <197594452+Aki-rabbit@users.noreply.github.com>
2025-06-20 16:33:22 +0900

    acronymの解答

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -5,3 +5,10 @@ Write your code for the 'Acronym' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/acronym` directory.
 =end
+class Acronym
+    def self.abbreviate(phrase)
+        cleaned_string = phrase.gsub('-', ' ').gsub(/[^a-zA-Z\s]/, '') #入力の正規化
+        picked_up_words = cleaned_string.split.map { |word| word[0].upcase } #単語を分割し、各単語の最初の文字を大文字に変換
+        picked_up_words.join #大文字の文字を結合してアクロニムを作成
+    end
+end
