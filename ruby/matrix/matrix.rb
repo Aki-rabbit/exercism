d8ff5b5c6a97faaabd878c4179ec91cfe1b4aa57
kakehi aki <197594452+Aki-rabbit@users.noreply.github.com>
2025-06-27 13:41:07 +0900

    matrix 解答2

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -11,12 +11,18 @@ To get started with TDD, see the `README.md` file in your
 #各列を上から下へ、そして列を左から右へ読み進めた列のリスト
 
 class Matrix
+    INDEX_ADJUSTMENT = 1 #インデックス調整のための定数
+
+    private_constant :INDEX_ADJUSTMENT
+
     def initialize(input_string)
         @row = RearrangeStrings.make_row(input_string) 
         @column = RearrangeStrings.make_column(@row) 
     end
 
     class RearrangeStrings
+        private_class_method :new
+        
         def self.make_row(input_string)
             input_string.split("\n").map do |row_str|
                 row_str.split(' ').map(&:to_i)
@@ -31,10 +37,10 @@ class Matrix
     end
 
     def row(index)
-        @row[index - 1]
+        @row[index - INDEX_ADJUSTMENT]
     end
 
     def column(index)
-        @column[index - 1]
+        @column[index - INDEX_ADJUSTMENT]
     end
 end
