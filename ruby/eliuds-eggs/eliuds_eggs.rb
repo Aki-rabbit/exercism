c1696096043eb273322fa1316f4d7c8134c642a1
kakehi aki <197594452+Aki-rabbit@users.noreply.github.com>
2025-07-02 14:45:37 +0900

    EliudsEggs　解答

--- ruby/eliuds-eggs/eliuds_eggs.rb
+++ ruby/eliuds-eggs/eliuds_eggs.rb
@@ -5,3 +5,21 @@ Write your code for the 'Eliuds Eggs' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/eliuds-eggs` directory.
 =end
+
+#10進数で与えられた数値を２進数に変換
+#２進数表現における1の数（卵の数）をカウント
+
+
+class EliudsEggs
+    def self.egg_count(displayed_number)
+        binary_number = displayed_number.to_i.to_s(2) 
+
+        count = 0
+        binary_number.each_char do |char|
+            if char == '1'
+                count += 1 #2進数表現の各桁を確認し、1の数をカウント
+            end
+        end
+        return count
+    end
+end
