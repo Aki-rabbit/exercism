77ca4cc640c8643f76103b05d805f204d0a46cac
kakehi aki <197594452+Aki-rabbit@users.noreply.github.com>
2025-07-02 16:15:59 +0900

    isogram.rb

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -5,3 +5,11 @@ Write your code for the 'Isogram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/isogram` directory.
 =end
+
+class Isogram
+
+    def self.isogram?(input)
+        cleaned_alphabet = input.downcase.gsub(/[^a-z]/, '') #小文字に変換し、アルファベット以外の文字を削除
+        cleaned_alphabet.chars.uniq.length == cleaned_alphabet.length #重複した文字を削除した文字数と全体の文字数を比較
+    end
+end
