5ebdeede1055201f8ff6d4c297c93850ad7dbab3
筧有紀 <kakehiaki@kakeiakinoMacBook-Air.local>
2025-07-14 13:49:18 +0900

    luhnの解答

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,7 +1,47 @@
-=begin
-Write your code for the 'Luhn' exercise in this file. Make the tests in
-`luhn_test.rb` pass.
+class Luhn
+    def self.valid?(input_string)
+        return false if input_string.nil? || input_string.empty?
+        return false if input_string.match?(/[^\d\s]/)
 
-To get started with TDD, see the `README.md` file in your
-`ruby/luhn` directory.
-=end
+        cleaned_digits = input_string.gsub(/\s/, '')
+
+        return false if cleaned_digits.length <= 1
+
+        sum_of_digits = self.calculate_sum(cleaned_digits)
+
+        self.is_valid_checksum?(sum_of_digits)
+    end
+
+    def self.calculate_sum(cleaned_string)
+        sum = 0
+        len = cleaned_string.length
+
+        cleaned_string.chars.each_with_index do |char, index|
+            digit = char.to_i
+
+            # 末尾からの位置を計算
+            position_from_end = len - index
+
+            # 末尾からの位置が偶数であれば対象の数字
+            if position_from_end.even? 
+                doubled_digit = digit * 2
+                sum += self.apply_luhn_doubling_rule(doubled_digit)
+            else # 末尾からの位置が奇数であればそのまま加算
+                sum += digit
+            end
+        end
+        sum
+    end
+
+    def self.apply_luhn_doubling_rule(number)
+        if number > 9
+            number - 9
+        else
+            number
+        end
+    end
+
+    def self.is_valid_checksum?(sum)
+        sum % 10 == 0
+    end
+end
